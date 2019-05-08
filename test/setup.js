387f33e1384177329e4aebe7d92ee4ef9cc53432
David Nordeen <davidnordeen@gmail.com>
2019-05-08 14:21:07 -0700

    completed post and delete

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,10 @@
 'use strict';
 
+
+process.env.TZ = 'UTC';
+process.env.NODE_ENV = 'test';
+
+require('dotenv').config();
 const { expect } = require('chai');
 const supertest = require('supertest');
 
